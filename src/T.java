2e1c1a73d56a24dbba0299b8da495a0ebef1469e
黄天浩 <891797651@qq.com>
2020-06-29 23:01:55 +0800

    提交火车初始程序

---
+++ src/T.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setSize(600,800);
+        f.setResizable(false);
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
